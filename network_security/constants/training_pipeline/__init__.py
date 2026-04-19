49b8ad9382a1380c9d3fd5cb5ad3784a8c3dac0d
unknown <samuel.villa.smith@hotmail.com>
2026-04-19 18:38:49 -0500

    data ingestion config files added

---
+++ network_security/constants/training_pipeline/__init__.py
@@ -0,0 +1,28 @@
+import os 
+import sys
+import numpy as np
+import pandas as pd
+
+
+"""
+Defining common constant variables for the training pipeline. This file will be imported in the training pipeline component of the training pipeline.
+"""
+
+TARGET_COLUMN: str = "Result"
+PIPELINE_NAME: str = "network_security_training_pipeline"
+ARTIFACT_DIR: str = "artifacts"
+FILE_NAME: str = "NetworkData.csv"
+
+TRAIN_FILE_NAME: str = "train.csv"
+TEST_FILE_NAME: str = "test.csv"
+
+"""
+Data ingestion related constants
+"""
+
+DATA_INGESTION_COLLECTION_NAME: str = "NetworkData"
+DATA_INGESTION_DATABASE_NAME: str = "NetworkSecurity"
+DATA_INGESTION_DIR_NAME: str = "data_ingestion"
+DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
+DATA_INGESTION_INGESTED_DIR: str = "ingested"
+DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO: float = 0.2
